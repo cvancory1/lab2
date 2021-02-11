1d1f8ba1ef79c8436000cb42b7586660c64e2fc4
cvancory1 <61095770+cvancory1@users.noreply.github.com>
2021-02-10 22:06:57 -0500

    Add files via upload

--- lab2.cpp
+++ lab2.cpp
@@ -4,9 +4,9 @@ Chloe VanCory
 COSC 320 
 Lab2
 
-Goals: Quicksort, partion function and swap. Mergesort with the merge function
+Goals: Quicksort, partition function and swap. Mergesort with the merge function
 
-
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./main look
 
 */
 
@@ -23,23 +23,25 @@ using namespace std;
 void printArray(int* A, int length);
 bool isSorted(int* arr, int n);
 
-int* makeArrRandom(int length);
-int* makeArrSorted(int length);
-int* makeArrBackward(int length);
-int* makeArrDuplicates(int length);
 
-int* arrCopy(int* A, int length);
+void makeArrRandom(int * arr, int length);
+void makeArrSorted(int * arr,int length);
+void makeArrBackward(int * arr,int length);
+void makeArrDuplicates(int * arr,int length);
+
+void arrCopy(int* source , int * dest, int length);
+
 
 void swap(int & num1, int& num2);
 int partition(int* arr, int start, int end, long &numSwaps); // big O n
 void quicksort(int* arr,int start,int end, long & numSwaps);
 
 int * merge(int * arr1, int * arr2, int length1, int length2, long & numCompared);
-int * mergesort(int * arr, int start, int end);
+int * mergesort(int * arr, int start, int end,long & numCompared);
 
 
-void timeMergesort( int * arr, int start, int end);
-void timeQuicksort( int * arr, int start, int end,long & numSwaps);
+void timeMergesort( int * arr, int start, int end, long & numCompared);
+void timeQuicksort( int * arr, int start, int end, long & numSwaps);
 
 
 
@@ -66,35 +68,79 @@ int main(){
     int length=4;
     int start_position=0;
     long numSwaps=0;
+    long numCompared;
+
+   
+    int * arrRan1= new int [length];
+    int * arrRan2= new int [length];
+    makeArrRandom(arrRan1,length);
+    arrCopy(arrRan1,arrRan2,length);
+
+    // timeMergesort(arrRan1, start_position, length,numCompared);
+    // timeQuicksort(arrRan2,start_position,length, numSwaps);
 
 
-    int * arrRan1= makeArrRandom(length);
+    // int * arrSort1= new int [length];
+    // int * arrSort2= new int [length];
+    // makeArrSorted(arrSort1,length);
+    // arrCopy(arrSort1,arrSort2,length);
 
-    int * arrRan2= arrCopy(arrRan1, length);
-
-    timeMergesort(arrRan1, start_position, length);
-    timeQuicksort(arrRan2,start_position,length, numSwaps);
+    // timeMergesort(arrSort1, start_position, length,numCompared);
+    // timeQuicksort(arrSort2,start_position,length, numSwaps);
 
 
+    // int * arrBackward1 = new int [length];
+    // int * arrBackward2 = new int [length];
+    // makeArrBackward(arrBackward1,length );
+    // arrCopy(arrBackward1,arrBackward2,length );
 
-
-    // int * arr= new int [3];
-    // int * arr1= new int [3];
-    // for(int i=0;i<3;i++){
-    //     cin>> arr[i];
-    // }
-
-    //  for(int i=0;i<3;i++){
-    //     cin>> arr1[i];
-    // }
-
-    // merge(arr,arr1,3,3);
+    // timeMergesort(arrBackward1, start_position, length,numCompared);
+    // timeQuicksort(arrBackward2,start_position,length, numSwaps);
+    
 
 
 
+    // int * makeArrDuplicates1= new int [length];
+    // int * makeArrDuplicates2= new int [length];
+    // makeArrSorted(makeArrDuplicates1,length);
+    // arrCopy(makeArrDuplicates1, makeArrDuplicates2,length);
+
+    // timeMergesort(makeArrDuplicates1, start_position, length,numCompared);
+    // timeQuicksort(makeArrDuplicates2,start_position,length, numSwaps);
+    
 
 
-}
+
+
+
+    // puts("");
+    // puts("");
+    // puts("");
+    // puts("");
+
+    // printArray(arrRan1,length);
+    // printArray(arrRan2,length);
+    // printArray(arrBackward1,length);
+    // printArray(arrBackward2,length);
+    // printArray(arrSort1,length);
+    // printArray(arrSort2,length);
+    // printArray(makeArrDuplicates1,length);
+    // printArray(makeArrDuplicates2,length);
+
+
+    delete arrRan1;
+    delete arrRan2;
+
+    // delete [] arrRan1;
+    // delete [] arrRan2;
+    // delete [] arrBackward1;
+    // delete [] arrBackward2;
+    // delete [] arrSort1;
+    // delete [] arrSort2;
+    // delete [] makeArrDuplicates1;
+    // delete [] makeArrDuplicates2;
+
+   }
 
 
 
@@ -130,7 +176,7 @@ bool isSorted(int* arr, int n){
     for(int i=1;i<n;i++){
         if(arr[i-1]>arr[i]){
             return false;
-            printArray(arr,n);
+            // printArray(arr,n);
         }
     }
     return true;
@@ -138,9 +184,8 @@ bool isSorted(int* arr, int n){
 }
 
 
-int* makeArrRandom(int length){
+void makeArrRandom(int * arr, int length){
 
-    int *arr= new int[length];
     for(int i=0;i<length;i++){
         arr[i]= rand() % 1000+ 2; 
     }
@@ -152,31 +197,25 @@ int* makeArrRandom(int length){
     //     arr[i]= num; 
     // }
      
-    return arr;
 }
 
-int* makeArrSorted(int length){
-    int *arr= new int[length];
+void makeArrSorted(int * arr ,int length){
      for(int i=0;i<length;i++){
          arr[i]=i;
      }
-     return arr;
 
 }
 
-int* makeArrBackward(int length){
-    int *arr= new int[length];
+void makeArrBackward(int * arr,int length){
     int backwards=length;
      for(int i=0;i<length;i++){
          arr[i]= backwards;
          backwards--;
      }
-     return arr;
 }
 
 // creates an array where half of the numbers are 30 and the other half are random
-int* makeArrDuplicates(int length){
-    int *arr= new int[length];
+void makeArrDuplicates(int * arr,int length){
     for(int i=0;i<length;i++){
         if(i%2 == 0){
             arr[i]=30;
@@ -185,33 +224,29 @@ int* makeArrDuplicates(int length){
         }
         
     }
-    return arr;
 }
 
 
 
-int* arrCopy(int* A, int length){
-    int * arr= new int[length];
+void arrCopy(int* source , int * dest, int length){
     
     for(int i=0;i<length;i++){
-        arr[i]= A[i];
+        dest[i]= source[i];
     }
 
-    return arr;
 }
 
 
 
 void swap(int & num1, int& num2){
-    cout<<"start  SWAP : num1="<<num1<<" "<<num2<<endl;
+    // <<"start  SWAP : num1="<<num1<<" "<<num2<<endl;
 
     int temp1=num1;
     int temp2=num2;
     num1= temp2;
     num2=temp1;
 
-    cout<<"END OF SWAP : num1="<<num1<<" "<<num2<<endl;
-    puts("");
+    // <<"END OF SWAP : num1="<<num1<<" "<<num2<<endl;
     
 
 }
@@ -222,25 +257,25 @@ int partition(int* arr, int start, int end, long & numSwaps){
     int loc = start-1;
    
 
-    puts("pivot and loc");
-    cout<<pivot<<" "<< loc<<endl;
+    // // puts("pivot and loc");
+     //cout<<pivot<<" "<< loc<<endl;
 
     for( int i= start; i < end-1; i++){
-        cout<<"i="<<i<<endl;
+        // // cout<<"i="<<i<<endl;
         if(arr[i] <= pivot){
-            cout<<" enter swap \n"<<endl;
+            // // cout<<" enter swap \n"<<endl;
             loc = loc +1;
             swap(arr[loc], arr[i]);
             numSwaps++;
         }
     }
-    cout<<"final swap arr[loc+1]= "<< arr[loc+1]<<" arr[end-1]="<< arr[end-1]<<endl;
+    // cout<<"final swap arr[loc+1]= "<< arr[loc+1]<<" arr[end-1]="<< arr[end-1]<<endl;
     swap(arr[loc+1], arr[end-1]);
     return loc+1;
 }
 
 void quicksort(int* arr,int start,int end, long & numSwaps){
-    printArray(arr,end);
+    // printArray(arr,end);
     // sleep(2);
 
     if(start< end){
@@ -250,29 +285,22 @@ void quicksort(int* arr,int start,int end, long & numSwaps){
         quicksort(arr,pivot+1,end, numSwaps);
 
     }
-    puts("sorted arr");
-    printArray(arr,end);
+    // puts("sorted arr");
+    // printArray(arr,end);
 
 
-    if(isSorted(arr,end)==true){
-        cout<<"\nThe number of swaps is "<< numSwaps<<"\n"<<endl;
 
-    }else {
-         cout<<"NOT SORTED  "<< numSwaps<<"\n"<<endl;
-
-    }
-    return;
 }
 
 
 int* merge(int * arr1, int * arr2, int length1, int length2, long & numCompared){
-    cout<<"MERGE"<<endl;
-    cout<<"arr1Lenth= "<<length1<<" length2= "<<length2<<endl;
+    // cout<<"MERGE"<<endl;
+    // cout<<"arr1Lenth= "<<length1<<" length2= "<<length2<<endl;
 
-        cout<<"merge arr1= ";
-        printArray(arr1,length1);
-        cout<<" merge arr2= ";
-        printArray(arr2, length2);
+        // cout<<"merge arr1= ";
+        // printArray(arr1,length1);
+        // cout<<" merge arr2= ";
+        // printArray(arr2, length2);
 
 
     int position1=0;// keeps track of the indexes
@@ -284,42 +312,45 @@ int* merge(int * arr1, int * arr2, int length1, int length2, long & numCompared)
     while(position1 < length1 && position2 < length2){
         // sleep(2);
         // cout<<"here"<<endl;
-        numCompared++;
 
         if( arr1[position1] <= arr2[position2] ){
             arr3[ front++ ] = arr1[ position1++ ]; // increments both after using 
-            cout<<"enter first"<<endl; 
-
-
+            // cout<<"enter first"<<endl; 
             
         }else if( arr1[position1] > arr2[position2] ){
             arr3[ front++ ] = arr2[ position2++ ]; // increments both after using 
-            cout<<"enter second"<<endl; 
+            // cout<<"enter second"<<endl; 
 
         }
+        numCompared++;
+
        
     }
-     printArray(arr3,length1+ length2);
+    //  printArray(arr3,length1+ length2);
     // takes care of the case where oen side is completely sorted and " short circuits" the above
     // while loop
     while(position1 < length1){
         arr3[ front++ ] = arr1[ position1++ ]; // increments both after using 
-        cout<<"in while loop 2 arr3= "<<endl; 
-        printArray(arr3,length1+ length2);
+        // cout<<"in while loop 2 arr3= "<<endl; 
+        // printArray(arr3,length1+ length2);
         numCompared++;
 
     }
 
     while(position2< length2){
         arr3[ front++ ] = arr2[ position2++ ]; // increments both after using 
-        cout<<"in while loop 3 arr3= "<<endl; 
-        printArray(arr3,length1+ length2);
+        // cout<<"in while loop 3 arr3= "<<endl; 
+        // printArray(arr3,length1+ length2);
         numCompared++;
 
     }
 
-    cout<<"         FINAL ARR in merge function"<<endl; 
-        printArray(arr3,length1+ length2);
+    // cout<<"         FINAL ARR in merge function"<<endl; 
+        // printArray(arr3,length1+ length2);
+
+    delete arr1;
+    delete arr2;
+
 
     return arr3;
 
@@ -330,90 +361,119 @@ int* merge(int * arr1, int * arr2, int length1, int length2, long & numCompared)
 }
 
 
-int * mergesort(int * arr, int start, int end){
-        long numCompared;
-        cout<<" \nEnter MERGESORT start = "<<start <<" end= "<<end<<endl;
-        // sleep(1/2);
-        printArray(arr, end);
+int * mergesort(int * arr, int start, int end, long& numCompared){
+        // cout<<" \nEnter MERGESORT start = "<<start <<" end= "<<end<<endl;
+        // sleep(1);
+        // printArray(arr, end);
          int mid = (start + end )/ 2;
 
     if(mid!=0){// arr is not empty
     // if(start< end){
        
-        cout<<" mid= "<<mid<<endl;
+        // cout<<" mid= "<<mid<<endl;
 
-        int * arr2= arrCopy ( arr, mid-start );
+        int * arr2= new int[ mid-start ];
         int * arr3= new int[ end-mid ];
 
-        int j=0;
-        for(int i= mid ; i< end; i++){
-            arr3[j++] = arr[i];
+
+        int j=0,k=0;
+        for(int i= 0 ; i< end; i++){
+
+            if(i<mid){
+               arr2[j++] = arr[i];
+                // puts("first");
+                // cout<<i<<endl;
+            }else if (i>=mid ){
+               arr3[k++] = arr[i];
+                // puts("sec");
+                // cout<<i<<endl;
+
+
+            }   
+            
         }
 
-        cout<<"arr2= ";
-        printArray(arr2, mid-start);
-        cout<<"arr3= " ;
-        printArray(arr3, end-mid );
- 
-        cout<<"mid-start= "<<mid-start<<" end-mid= "<<end-mid<<endl;
 
-        arr2 = mergesort(arr2 , start , mid-start);
-        arr3 = mergesort(arr3 , start , end-mid);
+        // cout<<"arr2= ";
+        // printArray(arr2, mid-start);
+        // cout<<"arr3= " ;
+        // printArray(arr3, end-mid );
 
-          cout<<"mid-start= "<<mid-start<<" end-mid= "<<end-mid<<endl;
+        // cout<<"mid-start= "<<mid-start<<" end-mid= "<<end-mid<<endl;
 
-        puts("end of merge sort");
-        // for shorter version 
+        arr2 = mergesort(arr2 , start , mid-start,numCompared);
+        arr3 = mergesort(arr3 , start , end-mid, numCompared);
 
+        // cout<<"mid-start= "<<mid-start<<" end-mid= "<<end-mid<<endl;
+        // puts("end of merge sort");
+
+        delete arr;
         return merge( arr2, arr3, mid-start , end-mid, numCompared);
 
-      
+        delete [] arr2;
+        delete [] arr3;
 
      } else{
-        puts("ELSE");
+        // puts("ELSE");
+       
+
+
         return arr;
     }
 
-    puts("FINAL ARR");
-    printArray(arr,end);
-
-    if(isSorted(arr,end)==true){
-        cout<<"\nThe number of swaps is "<< numCompared<<"\n"<<endl;
-
-    }else {
-         cout<<"NOT SORTED  "<< numCompared<<"\n"<<endl;
-
-    }
+ 
     
 }
 
 
-void timeMergesort( int * arr, int start, int end){
+void timeMergesort( int * arr, int start, int end, long& numCompared){
+   puts("Entering Mergesort");
 
  
   auto begin = std::chrono::system_clock::now();
-  mergesort(arr, start, end );
+   mergesort(arr, start, end, numCompared );
+
 
   auto finish = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = finish-begin;
   std::time_t end_time = std::chrono::system_clock::to_time_t(finish);
   std::cout << " finished at " << std::ctime(&end_time)
-    << "elapsed time: " << elapsed_seconds.count() << " seconds\n\n";
+
+    << "elapsed time: " << elapsed_seconds.count() << " seconds";
+
+    if(isSorted(arr,end)==true){
+        cout<<"\nThe number of swaps for mergesort was "<< numCompared<<"\n"<<endl;
+        numCompared=0;
+    }else {
+         cout<<"NOT SORTED  "<< numCompared<<"\n"<<endl;
+
+    }
 
     
 }
 
 void timeQuicksort( int * arr, int start, int end, long &numSwaps){
-
+   puts("Entering Quicksort");
  
   auto begin = std::chrono::system_clock::now();
   quicksort(arr, start, end , numSwaps);
+
 
   auto finish = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = finish-begin;
   std::time_t end_time = std::chrono::system_clock::to_time_t(finish);
   std::cout << " finished at " << std::ctime(&end_time)
-    << "elapsed time: " << elapsed_seconds.count() << " seconds\n\n";
+    << "elapsed time: " << elapsed_seconds.count() << " seconds";
+
+    if(isSorted(arr,end)==true){
+        cout<<"\nThe number of swaps for QuickSort was "<< numSwaps<<"\n"<<endl;
+        numSwaps=0;
+
+    }else {
+         cout<<"NOT SORTED  "<< numSwaps<<"\n"<<endl;
+
+    }
 
     
 }
+
