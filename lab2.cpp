1070aa81ba7292950c650c20d9a4d33798663824
cvancory1 <61095770+cvancory1@users.noreply.github.com>
2021-02-10 22:13:46 -0500

    Add files via upload

--- lab2.cpp
+++ lab2.cpp
@@ -129,11 +129,9 @@ int main(){
     // printArray(makeArrDuplicates2,length);
 
 
-    // delete arrRan1;
-    // delete arrRan2;
 
-    delete [] arrRan1;
-    delete [] arrRan2;
+    delete[] arrRan1;
+    delete[] arrRan2;
     // delete [] arrBackward1;
     // delete [] arrBackward2;
     // delete [] arrSort1;
